d88c02f16eb613b198d84221c9b9120366854d23
jerry_he <mac@jerry-hedeMacBook-Pro.local>
2025-11-07 17:26:55 +0800

    sd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,6 @@
 export default defineNuxtConfig({
   routeRules: {
-    // revalidated every 60 seconds, in the background
     '/**': { isr: 1800 },
-    // this page will be always fresh
-    '/dynamic': { isr: false },
-    // this page will be generated on demand and then cached permanently
-    '/static': { isr: true },
-    // this page is generated at build time and cached permanently
-    '/prerendered': { prerender: true },
-    // you can do lots more with route rules too!
-    '/redirect': { redirect: '/static' },
-    '/headers': { headers: { 'x-magic-of': 'nuxt and vercel' } },
-    '/spa': { ssr: false },
   },
   runtimeConfig: {
     nitro: { envPrefix: 'VERCEL_' },
